36dd012f0c2009fe375d1f5b2242faa177b6923f
petercharlesxp <petercharles.ca@hotmail.com>
2016-01-16 12:40:48 -0500

    before resharper

---
+++ Tests/ObservableObjectTests.cs
@@ -0,0 +1,48 @@
+﻿using EnterpriseMVVM.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tests
+{
+    [TestClass]
+    public class ObservableObjectTests
+    {
+        [TestMethod]
+        public void PropertyChangedEventHanderIsRaised()
+        {
+            var obj = new StubObservableObject();
+
+            bool raised = false;
+
+            obj.PropertyChanged += (sender, e) =>
+                {
+                    Assert.IsTrue(e.PropertyName == "ChangedProperty");
+                    raised = true;
+                };
+
+            obj.ChangedProperty = "Some Value";
+
+            if (!raised) Assert.Fail("PropertyChanged was never invoked.");
+        }
+
+        class StubObservableObject : ObservableObject
+        {
+            private string changedProperty;
+
+            public string ChangedProperty
+            {
+                get { return changedProperty; }
+                set
+                {
+                    changedProperty = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ Tests/ViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnterpriseMVVM.Windows;
+using System.ComponentModel;
+
+namespace Tests
+{
+    [TestClass]
+    public class ViewModelTests
+    {
+        [TestMethod]
+        public void IsAbstractBaseClass()
+        {
+            Type t = typeof(ViewModel);
+
+            Assert.IsTrue(t.IsAbstract);
+        }
+
+        [TestMethod]
+        public void IsIDataErrorInfo()
+        {
+            Assert.IsTrue(typeof(IDataErrorInfo).IsAssignableFrom(typeof(ViewModel)));
+        }
+    }
+}
