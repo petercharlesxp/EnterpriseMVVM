f91865abb0e891771841b309f8bbc149b8397e6d
petercharlesxp <petercharles.ca@hotmail.com>
2016-01-17 08:18:15 -0500

    suceess in CanCreateDatabase

---
+++ Tests/EnterpriseMVVM.Windows.Tests/UnitTests/ViewModelTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnterpriseMVVM.Windows;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnterpriseMVVM.Windows.Tests.UnitTests.Tests
+{
+    [TestClass]
+    public class ViewModelTests
+    {
+        [TestMethod]
+        public void IsAbstractBaseClass()
+        {
+            Type t = typeof(ViewModel);
+
+            Assert.IsTrue(t.IsAbstract);
+        }
+
+        [TestMethod]
+        public void IsIDataErrorInfo()
+        {
+            Assert.IsTrue(typeof(IDataErrorInfo).IsAssignableFrom(typeof(ViewModel)));
+        }
+
+        [TestMethod]
+        public void IsObservableObject()
+        {
+            Assert.IsTrue(typeof(ViewModel).BaseType == typeof(ObservableObject));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void IDataErrorInfo_ErrorProperty_IsNotImplemented()
+        {
+            var viewModel = new StubViewModel();
+            var value = viewModel.Error;
+        }
+
+        [TestMethod]
+        public void IndexerPropertyValidatesPropertyNameWithInvalidValue()
+        {
+            var viewModel = new StubViewModel();
+            Assert.IsNotNull(viewModel["RequiredProperty"]);
+        }
+
+        [TestMethod]
+        public void IndexerPropertyValidatesPropertyNameWithValidValue()
+        {
+            var viewModel = new StubViewModel
+            {
+                RequiredProperty = "Some Value"
+            };
+            Assert.IsNull(viewModel["RequiredProperty"]);
+        }
+        class StubViewModel : ViewModel
+        {
+            [Required]
+            public string RequiredProperty
+            {
+                get; set;
+            }
+        }
+    }
+}
