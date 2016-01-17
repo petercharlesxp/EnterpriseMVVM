06fd8be0bd9ddb8687ca5a8db15c0489095612d4
petercharlesxp <petercharles.ca@hotmail.com>
2016-01-17 12:25:51 -0500

    WPF Enterprise MVVM Session 2: Adding Data and Business Layers using Entity Framework

---
+++ Tests/EnterpriseMVVM.Data.Tests/FunctionalTests/CustomerScenarioTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseMVVM.Data.Tests.FunctionalTests
+{
+    [TestClass]
+    public class CustomerScenarioTests : FunctionalTest
+    {
+        [TestMethod]
+        public void AddNewCustomerIsPersisted()
+        {
+            //using (var dc = new DomainContext())
+            using (var bc = new BusinessContext())
+            {
+                Customer entity = bc.AddNewCustomer("David", "Anderson");
+
+                bool exists = bc.DataContext.Customers.Any(c => c.Id == entity.Id);
+
+                Assert.IsTrue(exists);
+            }
+        }
+    }
+}
---
+++ Tests/EnterpriseMVVM.Data.Tests/UnitTests/BusinessContextTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseMVVM.Data.Tests.UnitTests
+{
+    [TestClass]
+    public class BusinessContextTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddNewCustomer_ThrowsExpection_WhenFirstNameIsNull()
+        {
+            using (BusinessContext bc = new BusinessContext())
+            {
+                bc.AddNewCustomer(null, "Anderson");
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddNewCustomer_ThrowsExpetion_WhenFirstNameIsEmpty()
+        {
+            using (var bc = new BusinessContext())
+            {
+                bc.AddNewCustomer("", "Anderson");
+            }
+        }
+    }
+}
